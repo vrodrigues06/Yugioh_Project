fd2358fb3bae6df53b59911bf632a1ed54c35b5e
V.Rodrigues <vitorrodrigues06@outlook.com>
2025-06-13 17:50:52 -0300

    adicionado invalidateQueries no hook useCreatePersonagem

--- src/hooks/useCreatePersonagem.tsx
+++ src/hooks/useCreatePersonagem.tsx
@@ -1,10 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { createPersonagem } from "../api/apiPersonagens";
 import toast from "react-hot-toast";
 import { Personagem } from "../@types/personagem"; // Corrigido para usar Personagem
 import { FieldValues } from "react-hook-form";
 
 export default function useCreatePersonagem() {
+  const queryClient = useQueryClient();
   const { isPending: isCreating, mutate } = useMutation<
     Personagem | undefined,
     Error,
@@ -13,6 +14,9 @@ export default function useCreatePersonagem() {
     mutationFn: createPersonagem,
     onSuccess: () => {
       toast.success("Personagem Criado com sucesso");
+
+      // Invalida a query para que ela seja refeita
+      queryClient.invalidateQueries({ queryKey: ["allPersonagens"] });
     },
     onError: (error) => {
       const errorMessage =
